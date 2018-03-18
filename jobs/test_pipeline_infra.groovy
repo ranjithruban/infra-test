5f4e32953e45b5e9c987ea057232ace4abd153fe
ranjithruban <ranjith.ruban@auto1.com>
2018-03-18 19:34:50 +0100

    Change and m2 folder

---
+++ jobs/test_pipeline_infra.groovy
@@ -0,0 +1,22 @@
+pipelineJob("test_pipeline") {
+
+    description("Test Declarative Pipeline")
+
+    //parameters {
+    //    stringParam('app', 'test-service', 'application name')
+    }
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github("ranjithruban/infra-test", 'https')
+                        branch('master')
+                    }
+                }
+            }
+            scriptPath('Jenkinsfile')
+        }
+    }
+}
