cf0cecdacf29c933383cb8dcb53f85f2bef88b0a
ranjithruban <ranjith.ruban@auto1.com>
2018-03-20 19:03:54 +0100

    changes

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,16 +4,18 @@
 make clean all
 
 #build the docker images
-REGISTRY="test"
+REGISTRY="ranjith"
 
 git_branch=`echo $BRANCH_NAME`
 echo $git_branch
 image_tag=`echo $SHORT_COMMIT`
 echo $image_tag
 
+sudo docker login --username=ranjithruban --password=fd1a384c-93d5-47a6-90b8-bbbecc9181e9
+
 for image in frontend quotes newsfeed static-assets; do
     echo "** Building docker image $image"
-    image_name="$REGISTRY/test-infra/$image"
+    image_name="$REGISTRY/$image"
     image_id=`sudo docker build -q --force-rm -f docker/$image/Dockerfile .`
     echo "** Tagging image id $image_id"
     sudo docker tag $image_id $image_name:$image_tag
@@ -24,3 +26,6 @@ for image in frontend quotes newsfeed static-assets; do
     echo "** $image_name:latest pushed"
     echo "** $image_name:$image_tag pushed"
 done
+
+#upload the image to ECR or docker registry
+
