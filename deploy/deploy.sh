d5e70af3049c98641f47b7863cd6e832a64d5af4
ranjithruban <ranjith.ruban@auto1.com>
2018-03-20 20:04:07 +0100

    changes

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,7 +11,7 @@ echo $git_branch
 image_tag=`echo $SHORT_COMMIT`
 echo $image_tag
 
-sudo docker login --username=ranjithruban --password=${password}
+sudo docker login -u ranjithruban -p ${password}
 
 for image in frontend quotes newsfeed static-assets; do
     echo "** Building docker image $image"
